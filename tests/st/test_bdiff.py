32101374be74437a8c39e64cd21326465a9aae57
Zhikang Yan <2951256653@qq.com>
2025-10-16 21:12:41 +0800

    test(misc): move all tests to folder tests

--- tests/st/test_bdiff.py
+++ tests/st/test_bdiff.py
@@ -1,11 +1,13 @@
-import bdiff
 import os
 
-def test_bdiff():
-    base_diff_path = "../diff-cases/"
+import bdiff
+
+
+def test_bdiff() -> None:
+    base_diff_path = "./diff-cases/"
     left_files = sorted(os.listdir(base_diff_path + r"left_files"))
     right_files = sorted(os.listdir(base_diff_path + r"right_files"))
-    with open("../diff-cases/edit_scripts", 'r', encoding="utf8") as es_file:
+    with open(base_diff_path + "edit_scripts", 'r', encoding="utf8") as es_file:
         edit_scripts = eval(es_file.read())
     for left_file, right_file, expected_es in zip(left_files, right_files, list(edit_scripts.values())):
         print(left_file)
